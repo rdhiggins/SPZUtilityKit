31e1ee97d572556c723d6038cd49700a6d3d8cbd
Rodger Higgins <rdhiggins@gmail.com>
2015-05-26 11:31:54 -0500

    Added in URLEncode string extension

---
+++ SPZUtilityKit/URLEncode.swift
@@ -0,0 +1,21 @@
+//
+//  URLEncode.swift
+//  SPZUtilityKit
+//
+//  Created by Rodger Higgins on 5/26/15.
+//  Copyright (c) 2015 Spazstik Software. All rights reserved.
+//
+
+import Foundation
+
+public extension String {
+    public func urlEncode() -> String {
+        return CFURLCreateStringByAddingPercentEscapes(
+            nil,
+            self,
+            nil,
+            "!*'();:@&=+$,/?%#[]",
+            CFStringBuiltInEncodings.UTF8.rawValue
+            ) as String
+    }
+}
