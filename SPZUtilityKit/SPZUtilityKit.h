7834a400ee95cbac3b1c39ab5703eca001254848
Rodger Higgins <rdhiggins@gmail.com>
2015-05-26 10:42:03 -0500

    Initial Commit

---
+++ SPZUtilityKit/SPZUtilityKit.h
@@ -0,0 +1,19 @@
+//
+//  SPZUtilityKit.h
+//  SPZUtilityKit
+//
+//  Created by Rodger Higgins on 5/26/15.
+//  Copyright (c) 2015 Spazstik Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SPZUtilityKit.
+FOUNDATION_EXPORT double SPZUtilityKitVersionNumber;
+
+//! Project version string for SPZUtilityKit.
+FOUNDATION_EXPORT const unsigned char SPZUtilityKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPZUtilityKit/PublicHeader.h>
+
+
